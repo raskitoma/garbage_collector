735a5798491d305c7ee3728e5135780da0fbf4ff
Elias Achi <elias@plazacybersecurity.com>
2024-04-11 12:53:11 -0500

    debugh

--- garbage_collector.sh
+++ garbage_collector.sh
@@ -31,6 +31,7 @@ process_backups() {
     
     # Loop through files in the directory
     for file in *; do
+        log_message "Processing file: $file"
         # Check if the file matches the pattern (yyyy-mm-dd)
         if [[ $file =~ ^([0-9]{4}-[0-9]{2}-[0-9]{2}) ]]; then
             backup_date="${BASH_REMATCH[1]}"
